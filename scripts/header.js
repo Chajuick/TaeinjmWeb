d3ed087b5528d4498afcbea39a87391831ea07a8
Chajuick <139515945+Chajuick@users.noreply.github.com>
2026-01-22 09:31:42 +0900

    fix: [경로수정]

--- scripts/header.js
+++ scripts/header.js
@@ -1,17 +1,11 @@
 const header = document.getElementById('header');
 
-const isIndex =
-  location.pathname === '/' ||
-  location.pathname.endsWith('/index.html');
-
-function updateHeaderBase() {
+function updateHeaderBase(isIndex) {
   if (!isIndex || window.scrollY > 0) {
     header.classList.add('default');
   } else {
     header.classList.remove('default');
   }
 }
-
-updateHeaderBase();
 
 window.addEventListener('scroll', updateHeaderBase);
