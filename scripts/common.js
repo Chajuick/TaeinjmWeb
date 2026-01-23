2cd918ec8b85220080b88feeb0dac84ca764256f
Chajuick <139515945+Chajuick@users.noreply.github.com>
2026-01-23 17:45:50 +0900

    chore: [반응형] 레이아웃 설게 중

--- scripts/common.js
+++ scripts/common.js
@@ -1,11 +1,29 @@
-fetch('/TaeinjmWeb/components/header.html')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('header').innerHTML = html;
-  });
+function fetchWithFallback(primaryUrl, fallbackUrl, targetId) {
+  fetch(primaryUrl)
+    .then(res => {
+      if (!res.ok) throw new Error('primary failed');
+      return res.text();
+    })
+    .then(html => {
+      document.getElementById(targetId).innerHTML = html;
+    })
+    .catch(() => {
+      return fetch(fallbackUrl)
+        .then(res => res.text())
+        .then(html => {
+          document.getElementById(targetId).innerHTML = html;
+        });
+    });
+}
 
-fetch('/TaeinjmWeb/components/footer.html')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('footer').innerHTML = html;
-  });
+fetchWithFallback(
+  '/TaeinjmWeb/components/header.html',
+  '/components/header.html',
+  'header'
+);
+
+fetchWithFallback(
+  '/TaeinjmWeb/components/footer.html',
+  '/components/footer.html',
+  'footer'
+);
